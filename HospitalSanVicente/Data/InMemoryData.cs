928c0d537607cb0634ed7e6ad5aab18d566bb315
jorgeahenriqueznovoa <jorgeahenriqueznovoa@gmail.com>
2025-10-14 19:19:22 +0000

    feat(database): add database

---
+++ HospitalSanVicente/Data/InMemoryData.cs
@@ -0,0 +1,13 @@
+using HospitalSanVicente.Model;
+using System.Collections.Generic;
+
+namespace HospitalSanVicente.Data
+{
+    public static class InMemoryData
+    {
+        public static readonly List<Patient> Patients = new List<Patient>();
+        public static readonly List<Doctor> Doctors = new List<Doctor>();
+        public static readonly List<Appointment> Appointments = new List<Appointment>();
+        public static readonly List<EmailLog> EmailLogs = new List<EmailLog>();
+    }
+}
